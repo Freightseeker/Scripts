c22db59b78acbfdf1ca03611f1b07aa7b85aa26f
Mattias Jarestad <mattias@portfolion.se>
2020-08-11 10:44:04 +0200

    Update install-admin.sh
    Updated nvm to use node version 8.11.1.

--- install-admin.sh
+++ install-admin.sh
@@ -61,10 +61,11 @@ then
 	export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-	nvm uninstall --lts
-	nvm install --lts
-	nvm use --lts
+	nvm uninstall 8.11.1
+	nvm install 8.11.1
+	nvm use 8.11.1
+	nvm version
+	node --version
 fi
 
 ####################################################################
